2f9c4977c9bf9afc7abf221c8901233373a5195b
AnduinXue <anduin@aiursoft.com>
2024-03-17 17:14:01 +0000

    Simplify runner.

--- Runner/entry.sh
+++ Runner/entry.sh
@@ -4,9 +4,7 @@ token=$(cat /run/secrets/gitlab-runner-token)
 gitlab-runner register \
     --non-interactive \
     --url "https://gitlab.aiursoft.cn/" \
-    --request-concurrency 3 \
     --token $token
 
-fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 gitlab-runner start
 gitlab-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner
