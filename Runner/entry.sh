c447791a967b9cefe327fa54e99bd59e8e632f55
AnduinXue <anduin@aiursoft.com>
2024-03-31 07:48:05 +0000

    Reject emtpy token.

--- Runner/entry.sh
+++ Runner/entry.sh
@@ -1,4 +1,11 @@
 token=$(cat /run/secrets/gitlab-runner-token)
+
+# If token is empty, exit
+if [ -z "$token" ]; then
+    echo "No token provided."
+    exit 1
+fi
+
 echo "Registering runner with token $token"
 
 # One runner can run 3 tasks at the same time
