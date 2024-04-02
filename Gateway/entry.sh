25702b1240181204bf9b6ab2e75953e04b5f47d2
AnduinXue <anduin@aiursoft.com>
2024-04-02 15:20:20 +0000

    Xray requires user id.

---
+++ Gateway/entry.sh
@@ -0,0 +1,16 @@
+token=$(cat /run/secrets/user_id)
+
+# If token is empty, exit
+if [ -z "$token" ]; then
+    echo "No token provided."
+    exit 1
+fi
+
+echo "Setting up xray..."
+jq ".inbounds[0].settings.clients[0].id = \"$token\"" /etc/xray/config.json > /etc/xray/config.json.tmp
+
+echo "Replacing config file..."
+mv /etc/xray/config.json.tmp /etc/xray/config.json
+
+echo "Starting xray..."
+/usr/bin/xray -config /etc/xray/config.json
