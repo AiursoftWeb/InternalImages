e770d6df6c1cef06495786ff1c8ec0f4418e7211
AnduinXue <anduin@aiursoft.com>
2024-04-09 14:15:12 +0000

    Log more.

--- Runner/entry.sh
+++ Runner/entry.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -e
+
+# Load from environment variables
+gitlab_endpoint=${GITLAB_ENDPOINT:-http://gitlab}
+
+echo "Ensure gitlab server $gitlab_endpoint is reachable"
+while ! curl -s $gitlab_endpoint/ping; do
+    echo "Waiting for gitlab server..."
+    sleep 2
+done
+
+echo "Reading token from /run/secrets/gitlab-runner-token"
 token=$(cat /run/secrets/gitlab-runner-token)
 
 # If token is empty, exit
@@ -13,7 +24,7 @@ echo "Registering runner with token $token"
 # One runner can run 1 tasks at the same time
 gitlab-runner register \
     --non-interactive \
-    --url "http://gitlab/" \
+    --url "$gitlab_endpoint" \
     --token $token --executor "shell"
 
 gitlab-runner start
