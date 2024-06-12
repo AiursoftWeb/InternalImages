87a7ec1f0ee0afd07200c3b8f17d6742b366f2d1
Anduin <anduin@aiursoft.com>
2024-06-12 16:23:15 +0000

    Update entry.sh to include additional options for youtube-dl

--- Youtubedl/entry.sh
+++ Youtubedl/entry.sh
@@ -36,7 +36,24 @@ for url in "${user_urls[@]}"; do
     echo "Starting download $url"
     date
     # Start a new tmux session for the channel
-    tmux new -d -s "$user_id" "youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --add-metadata --embed-thumbnail --sleep-interval 2 --max-sleep-interval 10 -i -o '/mnt/data/youtube/%(uploader)s/%(title)s.%(ext)s' $url"
+    tmux new -d -s "$user_id" "\
+youtube-dl \
+    -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' \
+    --download-archive '/mnt/data/youtube/$user_id.txt' \
+    --write-description \
+    --write-info-json \
+    --write-annotations \
+    --write-thumbnail \
+    --write-sub \
+    --all-subs \
+    --ignore-errors \
+    --merge-output-format mp4 \
+    --add-metadata \
+    --embed-subs \
+    --embed-thumbnail \
+    --sleep-interval 50 \
+    --max-sleep-interval 100 \
+    -o '/mnt/data/youtube/%(uploader)s/%(title)s.%(ext)s' $url"
     sleep 800
 done
 
