b8cd8418ecdd9efee10d426d0363054cb76951e0
Anduin Xue <anduin@aiursoft.com>
2025-03-09 09:59:01 +0000

    Add new user URL for MagicSecretsRevealed to entry.sh

--- Youtubedl/entry.sh
+++ Youtubedl/entry.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 user_urls=(
     "https://www.youtube.com/@redknot-miaomiao/videos"
+    "https://www.youtube.com/@MagicSecretsRevealed/videos"
     "https://www.youtube.com/@HsWrWwc/videos"
     "https://www.youtube.com/@thu4878/videos"
     "https://www.youtube.com/@user-lk3gk5sd7n/videos"
