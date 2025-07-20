096da848fe3c94d7977c44debae96df06d63718e
Anduin Xue <anduin@aiursoft.com>
2025-07-20 04:36:14 +0000

    Add environment variable support for Docker socket and network configuration in runner registration

--- Runner/entry.sh
+++ Runner/entry.sh
@@ -15,6 +15,18 @@ else
     exit 1
 fi
 
+job_docker_to_use_socket=${JOB_DOCKER_TO_USE_SOCKET}
+if [ -z "$job_docker_to_use_socket" ]; then
+    echo "JOB_DOCKER_TO_USE_SOCKET is not set. Exiting..."
+    exit 1
+fi
+
+job_container_in_network=${JOB_CONTAINER_IN_NETWORK}
+if [ -z "$job_container_in_network" ]; then
+    echo "JOB_CONTAINER_IN_NETWORK is not set. Exiting..."
+    exit 1
+fi
+
 # Load from environment variables
 gitlab_endpoint=${GITLAB_ENDPOINT:-http://gitlab}
 
@@ -36,13 +48,21 @@ fi
 echo "Registering runner with token $token with name $HOSTNAME"
 
 # One runner can run 1 tasks at the same time
+
+# Use host docker socket to start job docker containers
+# However, in job docker containers, if the job needs to run docker commands, it should use the docker socket in the job container. Configured by JOB_DOCKER_TO_USE_SOCKET
+# For example, if JOB_DOCKER_TO_USE_SOCKET is set to /var/run/docker.sock, then the job container will use the host's docker socket.
+# For example, if JOB_DOCKER_TO_USE_SOCKET is set to tcp://docker:2375, then the job container will use the docker socket in the dind container.
 gitlab-runner register \
     --name $HOSTNAME \
     --non-interactive \
     --url "$gitlab_endpoint" \
     --token $token \
     --executor "docker" \
+    --env "DOCKER_HOST=$job_docker_to_use_socket \
     --docker-image "hub.aiursoft.cn/aiursoft/internalimages/jobrunner:latest" \
+    --docker-network-mode "$job_container_in_network" \
+    --docker-volumes "/certs/client" \
     --custom_build_dir-enabled=true
 
 # Inject `    enabled = true` under `[[runners.custom_build_dir]]` in config.toml
