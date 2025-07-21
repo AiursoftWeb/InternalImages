2525c4fad1d749681fcb0adecab9259b14add158
Anduin Xue <anduin@aiursoft.com>
2025-07-21 07:28:20 +0000

    Remove deploy_job_runner job and associated Dockerfile, streamline entry script for GitLab Runner

--- Runner/entry.sh
+++ Runner/entry.sh
@@ -13,25 +13,6 @@ else
     exit 1
 fi
 
-# JOB_CONTAINER_IMAGE
-job_container_image=${JOB_CONTAINER_IMAGE}
-if [ -z "$job_container_image" ]; then
-    echo "JOB_CONTAINER_IMAGE is not set. Exiting..."
-    exit 1
-fi
-
-job_docker_to_use_socket=${JOB_DOCKER_TO_USE_SOCKET}
-if [ -z "$job_docker_to_use_socket" ]; then
-    echo "JOB_DOCKER_TO_USE_SOCKET is not set. Exiting..."
-    exit 1
-fi
-
-job_container_in_network=${JOB_CONTAINER_IN_NETWORK}
-if [ -z "$job_container_in_network" ]; then
-    echo "JOB_CONTAINER_IN_NETWORK is not set. Exiting..."
-    exit 1
-fi
-
 # Load from environment variables
 gitlab_endpoint=${GITLAB_ENDPOINT:-http://gitlab}
 
@@ -40,6 +21,7 @@ if ! curl -s $gitlab_endpoint; then
     echo "GitLab server is not reachable. Exiting..."
     exit 1
 fi
+echo "GitLab server: $gitlab_endpoint is reachable"
 
 echo "Reading token from /run/secrets/gitlab-runner-token"
 token=$(cat /run/secrets/gitlab-runner-token)
@@ -52,23 +34,13 @@ fi
 
 echo "Registering runner with token $token with name $HOSTNAME"
 
-# One runner can run 1 tasks at the same time
-
-# Use host docker socket to start job docker containers
-# However, in job docker containers, if the job needs to run docker commands, it should use the docker socket in the job container. Configured by JOB_DOCKER_TO_USE_SOCKET
-# For example, if JOB_DOCKER_TO_USE_SOCKET is set to /var/run/docker.sock, then the job container will use the host's docker socket.
-# For example, if JOB_DOCKER_TO_USE_SOCKET is set to tcp://docker:2375, then the job container will use the docker socket in the dind container.
+echo "Using job docker socket for job containers: $job_docker_to_use_socket"
 gitlab-runner register \
     --name $HOSTNAME \
     --non-interactive \
     --url "$gitlab_endpoint" \
     --token $token \
-    --executor "docker" \
-    --env "DOCKER_HOST=$job_docker_to_use_socket" \
-    --env "DOCKER_TLS_CERTDIR=" \
-    --docker-image "$job_container_image" \
-    --docker-network-mode "$job_container_in_network" \
-    --docker-volumes "/certs/client" \
+    --executor "shell" \
     --custom_build_dir-enabled=true
 
 #gitlab-runner start
